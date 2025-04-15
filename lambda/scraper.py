05fdfd22052c694fc147909395d7b1f2955992a1
isaac-fletcher <ifletch@clemson.edu>
2025-04-14 20:18:37 -0400

    Updated IAM and Lambda for scraper

--- lambda/scraper.py
+++ lambda/scraper.py
@@ -2,82 +2,120 @@ import os
 import io
 import json
 import urllib3
-import boto3
 import scrapy
 import logging
+import hashlib
+import boto3, botocore
 from scrapy.crawler import CrawlerProcess
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin, quote_plus
 
 # AWS S3 Configuration
 DOCUMENTATION_BUCKET = "palmetto-docs"
 POLICY_BUCKET = "ccit-docs"
 GITHUB_S3_FOLDER = "github-md-files"
 WEBSITE_S3_FOLDER = "website-html-files"
-HARDWARE_S3_FOLDER = "lambda-responses"
 
-KNOWLEDGE_BASES = [
-    'OACW6QTO3Q',
-    'QTOUTNGPGG'
-]
+AURORA_SLEEP_WAIT_TIME = 5
 
-DATA_SOURCES = [
-    '5ESCMTRS6X',
-    'NJIKFBA9AH'
-]
+# Dictionary of Knowledge Bases to sync
+# Format: Knowledge Base ID: [Data Sources to Sync]
+KNOWLEDGE_BASES = {
+    'OACW6QTO3Q': ['5ESCMTRS6X'],
+    'QTOUTNGPGG': ['NJIKFBA9AH']
+}
 
+# Paths for Scraper to Ignore
+# Format: Site: [Paths]
+IGNORED_PATHS = {
+    "slurm.schedmd.com": ["/archive"],
+}
+
+# Content Types the Scraper will Accept
 ACCEPTED_CONTENT_TYPES = [
     'text/html',
     'text/plain',
     'text/markdown',
     'text/csv',
-    'application/pdf'
 ]
 
+# Valid File Extensions
 ACCEPTED_FILE_EXTENSIONS = [
     '.html',
     '.txt',
     '.md',
     '.csv',
-    '.pdf'
 ]
 
+
 DOCUMENTATION_REPOS = [
-    "https://github.com/clemsonciti/palmetto-examples"
+    #"https://github.com/clemsonciti/palmetto-examples"
 ]
-    
+
 DOCUMENTATION_SITES = [
-    "https://docs.rcd.clemson.edu/",
-    "https://slurm.schedmd.com/",
-    "https://docs.globus.org/globus-connect-personal/",
-    "https://docs.globus.org/guides/tutorials/",
-    "https://clemsonciti.github.io/rcde_workshops/"
+    #"https://docs.rcd.clemson.edu/",
+    #"https://slurm.schedmd.com/",
+    #"https://docs.globus.org/globus-connect-personal/",
+    #"https://docs.globus.org/guides/tutorials/",
+    #"https://clemsonciti.github.io/rcde_workshops/"
 ]
 
 POLICY_SITES = [
-    "https://ccit.clemson.edu/cybersecurity/policy/",
+    #"https://ccit.clemson.edu/cybersecurity/policy/",
     "https://clemsonpub.cfmnetwork.com/"
 ]
 
+# Initial setup configuration
 http = urllib3.PoolManager()
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-# Adding lambda client
-lambda_client = boto3.client('lambda')
+s3_client = boto3.client('s3')
+
+def upload_to_s3(file_key: str, s3_bucket: str, data):
+    """
+    Uploads data to an s3 bucket with a specified key.
+    """
+    local_hash = hashlib.sha256(data).hexdigest()
+
+    try:
+        response = s3_client.head_object(Bucket=s3_bucket, Key=file_key)
+
+        remote_hash = response['Metadata'].get('sha256', '')
+
+        if local_hash == remote_hash:
+            logger.info(f"Skipped upload because {file_key} unchanged")
+            return
+        else:
+            logger.info(f"Uploading {file_key} because it has changed")
+
+    except Exception as e:
+        if isinstance(e, botocore.exceptions.ClientError) and e.response['Error']['Code'] == '404':
+            logger.info(f"Uploading {file_key} because it does not exist")
+        else:
+            raise
+
+    s3_client.upload_fileobj(
+            Fileobj=io.BytesIO(data),
+            Bucket=s3_bucket,
+            Key=file_key,
+            ExtraArgs={'Metadata': {'sha256': local_hash}}
+    )
+    logger.info(f"Uploaded {file_key} with hash {local_hash} to {s3_bucket}")
+
 
 def get_github_files(repo_url, file_type):
     """
     Fetches files of a specific type from a public GitHub repository.
     """
-    
+
     repo_api_url = repo_url.replace("github.com", "api.github.com/repos")
-    
+
     repo_response = http.request('GET', repo_api_url)
     if repo_response.status != 200:
         logger.info(f"Error fetching repository metadata: HTTP {repo_response.status}")
         return []
-    
+
     repo_data = json.loads(repo_response.data.decode('utf-8'))
     default_branch = repo_data.get("default_branch", "main")
 
@@ -87,10 +125,10 @@ def get_github_files(repo_url, file_type):
     if response.status != 200:
         logger.error(f"Error fetching repository data: HTTP {response.status}")
         return []
-    
+
     data = json.loads(response.data.decode('utf-8'))
     files = [file for file in data.get('tree', []) if file.get('type') == 'blob' and file.get('path', '').endswith(f".{file_type}")]
-    
+
     return files, default_branch
 
 def download_and_upload_github(repo_url_list, file_type):
@@ -98,27 +136,24 @@ def download_and_upload_github(repo_url_list, file_type):
     Downloads .md files from GitHub and uploads them to S3.
     """
     logger.info(f"\nDownloading {file_type} files from GitHub and uploading to S3...\n")
-    
+
     for repo_url in repo_url_list:
         files, default_branch = get_github_files(repo_url, file_type)
-        
-        s3_client = boto3.client('s3')
+
         for file in files:
             file_url = f"{repo_url}/raw/{default_branch}/{file['path']}"
-            file_response = http.request('GET', file_url)
+            response = http.request('GET', file_url)
 
-            if file_response.status != 200:
-                logger.error(f"Failed to fetch file: {file['path']} (HTTP {file_response.status})")
+            if response.status != 200:
+                logger.error(f"Failed to fetch file: {file['path']} (HTTP {response.status})")
                 continue
-            
-            file_content = file_response.data
-            
+
             parsed_repo = urlparse(repo_url)
+
             repo_name = parsed_repo.path.strip("/").split("/")[-1]
-            s3_key = os.path.join(GITHUB_S3_FOLDER, repo_name, file['path'])
-            s3_client.upload_fileobj(io.BytesIO(file_content), DOCUMENTATION_BUCKET, s3_key)
-            logger.info(f"Uploaded: {s3_key}")
-    
+            file_key = os.path.join(GITHUB_S3_FOLDER, repo_name, file['path'])
+            upload_to_s3(file_key, DOCUMENTATION_BUCKET, response.data)
+
         logger.info(f"Successfully uploaded {len(files)} {file_type} files to S3")
 
 class WebsiteSpider(scrapy.Spider):
@@ -135,52 +170,98 @@ class WebsiteSpider(scrapy.Spider):
     def parse(self, response):
         content_type = response.headers.get('Content-Type', b'').decode('utf-8')
 
-        # ignoring media 
+        # Ignoring invalid media
         if not any(valid_type in content_type for valid_type in ACCEPTED_CONTENT_TYPES):
             return
-        
-        parsed = urlparse(response.url)
-        url_path = os.path.dirname(parsed.path)
-        filename = os.path.basename(parsed.path) or "index"
-        
-        file_key = os.path.join(
-            WEBSITE_S3_FOLDER,
-            parsed.netloc + url_path,
-            filename if filename.endswith(tuple(ACCEPTED_FILE_EXTENSIONS)) else f"{filename}.html"
+
+        parsed_url = urlparse(response.url)
+
+        url_path = os.path.dirname(parsed_url.path)
+        url_endpoint = os.path.basename(parsed_url.path) or "index"
+
+        query_suffix = "_" + quote_plus(parsed_url.query) if parsed_url.query else ""
+
+        filename = (
+            # Use endpoint as filename if it has appropriate extension
+            url_endpoint if url_endpoint.endswith(tuple(ACCEPTED_FILE_EXTENSIONS))
+            # Otherwise use the endpoint and any applied query suffix
+            else f"{url_endpoint + query_suffix}.html"
         )
 
-        s3_client = boto3.client('s3')
-        s3_bucket = DOCUMENTATION_BUCKET if any(parsed.netloc in site for site in DOCUMENTATION_SITES) else POLICY_BUCKET
-        s3_client.upload_fileobj(io.BytesIO(response.body), s3_bucket, file_key)
-        logger.info(f"Uploaded {response.url} to S3 as {file_key}")
-        
+        file_key = os.path.join(
+            WEBSITE_S3_FOLDER,
+            parsed_url.netloc + url_path,
+            filename
+        )
+
+        s3_bucket = DOCUMENTATION_BUCKET if any(parsed_url.netloc in site for site in DOCUMENTATION_SITES) else POLICY_BUCKET
+
+        # Upload new or changed content
+        upload_to_s3(file_key, s3_bucket, response.body)
+
         for link in response.css("a::attr(href)").getall():
-            yield response.follow(link, callback=self.parse)
+            # Skipping recursive and invalid links
+            if link.startswith("#") or link.startswith("mailto:"):
+                continue
+
+            dest_url = response.urljoin(link)
+            parsed_dest_url = urlparse(dest_url)
+
+            # Prevents from crossing into separate subdomains
+            if parsed_dest_url.netloc not in self.allowed_domains:
+                logger.info(f"Skipping subdomain or external link: {dest_url}")
+                continue
+
+            # Prevents from traversing any ignored paths
+            for ignore in IGNORED_PATHS.get(parsed_dest_url.netloc, []):
+                if parsed_dest_url.path.startswith(ignore):
+                    self.logger.info(f"Skipping link to {dest_url}")
+                    break
+            else:
+                yield response.follow(link, callback=self.parse)
 
 def run_scraper(websites):
     process = CrawlerProcess({
         'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
         'LOG_LEVEL': 'INFO',
         'DEPTH_LIMIT': 5,
-        'CONCURRENT_REQUESTS': 32
+        'CONCURRENT_REQUESTS': 32,
+        'CONCURRENT_REQUESTS_PER_DOMAIN': 16,
     })
     process.crawl(WebsiteSpider, websites=websites)
     process.start()
 
 def sync_knowledgebases():
-    client = boto3.client('bedrock-agent')
-    
-    for kb, ds in zip(KNOWLEDGE_BASES, DATA_SOURCES):
-        response = client.start_ingestion_job(
-            knowledgeBaseId=kb,
-            dataSourceId=ds
-        )
+
+    while True:
+        try:
+            client = boto3.client('bedrock-agent')
+            logger.info("Syncing Knowledge Bases...")
+
+            for kb in KNOWLEDGE_BASES.keys():
+                for ds in KNOWLEDGE_BASES[kb]:
+                    response = client.start_ingestion_job(
+                        knowledgeBaseId=kb,
+                        dataSourceId=ds
+                    )
+                    logger.info(f"Data Store {ds} in Knowledge Base {kb} synced!")
+
+            return
+
+        except Exception as e:
+            # Handles dependencyFailedException raised when Aurora DB is paused
+            if isinstance(e, botocore.exceptions.ClientError) and e.response['Error']['Code'] == 'dependencyFailedException':
+                logger.error(f"Aurora DB auto-paused. Sleeping for {AURORA_SLEEP_WAIT_TIME} seconds to allow it to auto resume.")
+                sleep(AURORA_SLEEP_WAIT_TIME)
+            else:
+                logger.error(f"Flow {flow_id} failed to execute: {str(e)}")
+                raise e
 
 def lambda_handler(event, context):
-       
+
     download_and_upload_github(DOCUMENTATION_REPOS, "md")
     run_scraper(DOCUMENTATION_SITES + POLICY_SITES)
-    sync_knowledgebases()
+    # sync_knowledgebases()
 
     response = {
         "statusCode": 200,
@@ -190,4 +271,3 @@ def lambda_handler(event, context):
     logger.info("Response: %s", response)
 
     return response
-
